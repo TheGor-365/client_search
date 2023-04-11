ff035e5de17b0f610a548101c220d7bdb563b9c0
Gor <7495500@gmail.com>
2023-04-11 22:21:44 +0300

    Add client search filter with stimulus

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 50.times do
   Post.create(
     title: Faker::Lorem.sentence(word_count: 3),
-    content: Faker::Lorem.paragraph(sentence_count: 5)
+    content: Faker::Lorem.paragraph(sentence_count: 20)
   )
 end
